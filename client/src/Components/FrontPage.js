019bed7f4e9d8749dfeb9ef58678b9ea7a020ec8
Jussi Joki <judejoki@gmail.com>
2022-10-12 10:04:26 +0300

    hello world test

--- client/src/Components/FrontPage.js
+++ client/src/Components/FrontPage.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "./FrontPage.css"
 import TopMenu from "./TopMenu"
+import { useForm } from "react-hook-form"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+
+const FooBar = "Test"
 
 const FrontPage = () => {
   return (
     <div>
       <TopMenu />
-      Hello World!
+      Hello World! Updated!
     </div>
   )
 }
