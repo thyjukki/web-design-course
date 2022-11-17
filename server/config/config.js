80aa022368a3664d48b080ddc8b7c4b4615b2918
Jussi Joki <judejoki@gmail.com>
2022-11-17 20:41:49 +0200

    fixed env

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ export default {
     username: "root",
     password: process.env.MYSQL_ROOT_PASS,
     database: process.env.MYSQL_DB,
-    host: "localhost",
+    host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     dialect: "mysql"
   }
