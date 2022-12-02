5d442cc337a3aed4e473b2e4a18cff6ab4f49116
Lassi Knuuttila <lassi.knuuttila@hiq.fi>
2022-12-03 00:46:52 +0200

    Remove unused ProtectedRoute

--- client/src/Components/ProtectedRoute.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
-
-export const ProtectedRoute = ({ children }) => {
-  !localStorage.getItem("token") ? <Navigate replace to="/login" /> : children
-}
