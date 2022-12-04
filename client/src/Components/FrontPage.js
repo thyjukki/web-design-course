0c195811710a71810e6cd9eef5f18aca165e6af1
Aatos Saarinen <aatos@Aatos-MBP.wlan>
2022-12-04 19:06:30 +0200

    feat: front page facelift

--- client/src/Components/FrontPage.js
+++ client/src/Components/FrontPage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Course from "./common/Course"
 import { gql, useQuery } from "@apollo/client"
+import styled from "styled-components"
 
 const COURSES = gql`
   query {
@@ -16,43 +17,101 @@ const FrontPage = () => {
   const { loading, error } = useQuery(COURSES)
   error && console.error(error)
   return (
-    <div>
+    <Cont>
       {loading && <p>Loading</p>}
       {error && <p>Error: {error.message}</p>}
-      <Course
-        course={{
-          name: "User-Centered Methods for Product and Service Design",
-          credits: 5
-        }}
-        courseInstance={{
-          type: "Luento",
-          startDate: "5.9.2022",
-          endDate: "7.12.2022",
-          events: [
-            {
-              startDate: "5.9.2022",
-              endDate: "12.9.2022",
-              type: "Luento",
-              location: "T2 - C105 - Tietotekniikan talo"
-            },
-            {
-              startDate: "26.9.2022",
-              endDate: "10.10.2022",
-              type: "Luento",
-              location: "T2 - C105 - Tietotekniikan talo"
-            },
-            {
-              startDate: "24.10.2022",
-              endDate: "5.12.2022",
-              type: "Luento",
-              location: "T2 - C105 - Tietotekniikan talo"
-            }
-          ]
-        }}
-      />
       <Calendar />
-    </div>
+      <CurrentCourses>
+        <h2>Aktiiviset opinnot</h2>
+        {exampleCourses.map((course) => {
+          return (
+            <CourseCont key={course.code}>
+              <Course course={course} />
+            </CourseCont>
+          )
+        })}
+      </CurrentCourses>
+    </Cont>
   )
 }
+
+const exampleCourses = [
+  {
+    name: "User-Centered Methods for Product and Service Design",
+    credits: 5,
+    code: "TU-101",
+    courseInstance: {
+      type: "Luento",
+      startDate: "5.9.2022",
+      endDate: "7.12.2022",
+      events: [
+        {
+          startDate: "5.9.2022",
+          endDate: "12.9.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        },
+        {
+          startDate: "26.9.2022",
+          endDate: "10.10.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        },
+        {
+          startDate: "24.10.2022",
+          endDate: "5.12.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        }
+      ]
+    }
+  },
+  {
+    name: "Todennäköisyys ja tilastotiede",
+    credits: 5,
+    code: "MS-501",
+    courseInstance: {
+      type: "Luento",
+      startDate: "5.9.2022",
+      endDate: "7.12.2022",
+      events: [
+        {
+          startDate: "5.9.2022",
+          endDate: "12.9.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        },
+        {
+          startDate: "26.9.2022",
+          endDate: "10.10.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        },
+        {
+          startDate: "24.10.2022",
+          endDate: "5.12.2022",
+          type: "Luento",
+          location: "T2 - C105 - Tietotekniikan talo"
+        }
+      ]
+    }
+  }
+]
+
+const Cont = styled.div`
+  display: flex;
+  padding-top: 50px;
+`
+
+const CurrentCourses = styled.div``
+
+const CourseCont = styled.div`
+  padding: 2px;
+`
+
+const Padd = styled.div`
+  width: 100%;
+  height: 20px;
+`
 
 export default FrontPage
--- client/src/Components/common/Course.js
+++ client/src/Components/common/Course.js
@@ -2,30 +2,37 @@ import React from "react"
 import styled from "styled-components"
 import Accordion from "react-bootstrap/Accordion"
 import { Container } from "react-bootstrap"
+import Rand from "rand-seed"
 
 const Course = (props) => {
-  const { course, courseInstance } = props
+  const { course } = props
+  const { courseInstance } = course
 
   return (
-    <Container>
+    <BordContainer courseColor={getCourseColor(course.code)}>
       <Accordion defaultActiveKey="0">
-        <Header>
-          <Container>
-            <div>{course.name}</div>
-            <div>{`${courseInstance.startDate}-${courseInstance.endDate}`}</div>
-          </Container>
+        <Header courseColor={getCourseColor(course.code)}>
+          <Cont>
+            <Credits courseColor={getCourseColor(course.code)}>
+              {course.credits}
+            </Credits>
+            <Wrapper>
+              <div>{course.name}</div>
+              <div>{`${courseInstance.startDate}-${courseInstance.endDate}`}</div>
+            </Wrapper>
+          </Cont>
         </Header>
 
-        <Body>
+        <Body courseColor={getCourseColor(course.code)}>
           <Container>
             <div>{courseInstance.type}</div>
             <Lecture events={courseInstance.events} />
           </Container>
         </Body>
       </Accordion>
-    </Container>
+    </BordContainer>
   )
-};
+}
 
 const Lecture = (props) => {
   const { events } = props
@@ -42,15 +49,60 @@ const Lecture = (props) => {
       })}
     </Container>
   )
-};
+}
+
+const courseColors = ["#f1b963", "#dde0ab", "#97cba9", "#668ba4"]
+
+function getDecimalPart(num) {
+  if (Number.isInteger(num)) {
+    return 0
+  }
+
+  const decimalStr = num.toString().split(".")[1].slice(0, 3)
+  return Number(decimalStr)
+}
+
+const getCourseColor = (courseName) => {
+  const rand = new Rand(courseName)
+  return courseColors[getDecimalPart(rand.next()) % 4]
+}
+
+const BordContainer = styled.div`
+  border: 2px solid ${(p) => p.courseColor};
+`
 
 const Header = styled(Accordion.Header)`
-  border: 2px solid black;
   width: 400px;
+  height: 90px;
+  display: flex;
+`
+
+const Cont = styled.div`
+  display: flex;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`
+
+const Credits = styled.div`
+  background-color: ${(p) => p.courseColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 90px;
+  width: 20px;
 `
 
 const Body = styled(Accordion.Body)`
   max-width: 400px;
+  background-color: rgba(155, 155, 155, 0.2);
+  border-left: 20px solid ${(p) => p.courseColor};
 `
 
 export default Course
